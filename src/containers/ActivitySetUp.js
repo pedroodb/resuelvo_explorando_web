a498bb5b269b128e03e325aaefbf443591f8b5ca
pedroodb <dalbianco.pedro@gmail.com>
2019-03-12 18:46:21 -0300

    Prueba formulario con redux

--- src/containers/ActivitySetUp.js
+++ src/containers/ActivitySetUp.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { setTitle, setDescription } from '../actions/currentActivityActions'
 
 class ActivitySetUpContainer extends Component {
-    render() {
-      return (
-        <div>
-          <header> Creando actividad
-          </header>
-        </div>
-      )
-    }
+
+  render() {
+
+    console.log(this.props)
+      
+    const {
+      title,
+      description,
+    } = this.props.currentActivity
+
+    return (
+      <div>
+        <header> Creando actividad {title}
+        </header>
+        <input type="text" onChange={(event) => {this.props.actions.setTitle(event.target.value)}}></input>
+      </div>
+    )
   }
-  
-  export default ActivitySetUpContainer
+}
+
+//Funcion que mapea las acciones con las funciones que llamamos desde el componente
+function mapDispatchToProps(dispatch) {
+  return {
+    actions : bindActionCreators({
+      setTitle,
+      setDescription,
+    }, dispatch)
+  }
+}
+
+//Funcion que mapea el estado de la APLICACION (redux) con las props del componente
+function mapStateToProps({currentActivityReducer}) {
+  return {
+    currentActivity:currentActivityReducer
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ActivitySetUpContainer)
