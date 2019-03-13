cd4a4819d27939eb5bd80528eb8618cd71d61fd5
pedroodb <dalbianco.pedro@gmail.com>
2019-03-13 16:18:28 -0300

    Funciona rooteo desde boton

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 
 import logo from '../assets/resuelvo_explorando_logo.png'
 import '../styles/Home.css'
 
 class HomeContainer extends Component {
-    render() {
-      return (
-        <div id="HomeContainer">
-          <header className="header">
-            <img src={logo} className="logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save toooo reload.
-            </p>
-            <Link to="/activityCreation/activitySetUp">Crear actividad</Link>
-          </header>
-        </div>
-      )
-    }
+
+  render() {
+
+    const {
+      history
+    } = this.props
+
+    return (
+      <div id="HomeContainer">
+        <header className="header">
+          <img src={logo} className="logo" alt="logo" />
+          <p>
+            Edit src/App.js and save to reload.
+          </p>
+          <Button onClick={() => history.push("/activityCreation/activitySetUp")}>Crear actividad</Button>
+        </header>
+      </div>
+    )
   }
+}
   
-  export default HomeContainer
+export default withRouter(HomeContainer)
