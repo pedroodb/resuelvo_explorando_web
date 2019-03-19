a40a35e87b2fd6c8049b8a808ec1fb5c8515c26a
pedroodb <dalbianco.pedro@gmail.com>
2019-03-19 18:53:56 -0300

    Formulario modifica el estado del container taskSetUp

--- src/containers/TaskSetUp.js
+++ src/containers/TaskSetUp.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { Button, Form } from 'semantic-ui-react'
 
 class TaskSetUpContainer extends Component {
 
+  constructor(props) {
+    super(props)
+
+    this.handleFormChange = this.handleFormChange.bind(this)
+
+    this.state = {
+      ...this.props.task
+    }
+  }
+
+  handleFormChange(event) {
+    let newState = {}
+    newState[event.target.name] = event.target.value
+    this.setState(() => ({...newState}))
+  }
+
   render() {
+
+    const {
+      history
+    } = this.props
 
     return (
       <div>
         <header> Creando tarea
         </header>
+        <Form>
+          <Form.Field>
+            <label>Titulo</label>
+            <Form.Input name='title' placeholder='Titulo' onChange={this.handleFormChange} />
+          </Form.Field>
+          <Form.Field>
+            <label>Descripcion</label>
+            <input name='description' placeholder='Descripcion' onChange={this.handleFormChange} />
+          </Form.Field>
+          <Button onClick={() => history.push("/taskSetUp")}>Agregar tarea</Button>
+        </Form>
       </div>
     )
   }
@@ -27,7 +59,7 @@ function mapDispatchToProps(dispatch) {
 //Funcion que mapea el estado de la APLICACION (redux) con las props del componente
 function mapStateToProps({currentTaskReducer}) {
   return {
-    currentTask:currentTaskReducer
+    task:currentTaskReducer
   }
 }
 
