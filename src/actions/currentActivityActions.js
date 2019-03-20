b93aa99a8a5d821d84622393a3308db859c1f8fe
pedroodb <dalbianco.pedro@gmail.com>
2019-03-20 16:39:59 -0300

    Actions ahora son arrow functions

--- src/actions/currentActivityActions.js
+++ src/actions/currentActivityActions.js
@@ -1,29 +1,21 @@
 import { TITLE_SET, DESCRIPTION_SET, TASK_ADD, TASK_REMOVE } from '../constants'
 
-export function setTitle(name) {
-  return {
-    type:TITLE_SET,
-    payload:name,
-  }
-}
+export const setTitle = (name) => ({
+  type:TITLE_SET,
+  payload:name,
+})
 
-export function setDescription(description) {
-  return {
-    type:DESCRIPTION_SET,
-    payload:description,
-  }
-}
+export const setDescription = (description) => ({
+  type:DESCRIPTION_SET,
+  payload:description,
+})
 
-export function addTask(task) {
-  return {
-    type:TASK_ADD,
-    payload:task,
-  }
-}
+export const addTask = (task) => ({
+  type:TASK_ADD,
+  payload:task,
+})
 
-export function removeTask(task) {
-  return {
-    type:TASK_REMOVE,
-    payload:task,
-  }
-}
+export const removeTask = (task) => ({
+  type:TASK_REMOVE,
+  payload:task,
+})
--- src/actions/currentTaskActions.js
+++ src/actions/currentTaskActions.js
@@ -1,12 +1,10 @@
 import { TASK_SET, TASK_CLEAR } from '../constants'
 
-export function setTask(task) {
-  return {
-    type:TASK_SET,
-    payload:task,
-  } 
-}
+export const setTask = (task) => ({
+  type:TASK_SET,
+  payload:task,
+})
 
 export const clearTask = () => ({
-  type:TASK_CLEAR
+  type:TASK_CLEAR,
 })
