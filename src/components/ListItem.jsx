1388bac4007e812ca3c0d5d0dce9cb349a927518
pedroodb <dalbianco.pedro@gmail.com>
2019-11-04 13:24:58 -0300

    Eliminados componentes innecesarios

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,11 +3,11 @@ import { Button, List, Icon } from 'semantic-ui-react'
 
 const ListItem = ({name,load,del}) => (
   <List.Item>
-    <List.Header className='listHeader' >{name}</List.Header>
     <List.Content floated='right'>
       <Button primary onClick={load}> <Icon name='edit'/>Editar</Button>
       <Button primary onClick={del}><Icon name='trash'/>Eliminar</Button> 
     </List.Content>
+    <List.Content className='listHeader' >{name}</List.Content>
   </List.Item>  
 )
 
